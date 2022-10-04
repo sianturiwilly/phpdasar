653ddb00a6db6f2d3614e0be317c1af42840940f
sianturiwilly <prajudiwilliam10@gmail.com>
2022-10-04 23:46:56 +0700

    Commit ke-9

---
+++ database/phpdasar.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 04, 2022 at 06:43 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `nim` char(8) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `jurusan` varchar(100) DEFAULT NULL,
+  `gambar` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `email`, `jurusan`, `gambar`) VALUES
+(1, 'Willi Sianturi', '92316073', 'prajudiwilliam10@gmail.com', 'Sistem Informasi', 'willi.jpg'),
+(2, 'Darwis Purba', '91078933', 'darwispurba93@gmail.com', 'Hukum', 'darwis.jpg'),
+(3, 'Willi', '55411549', 'willymusermufc@hotmail.com', 'Teknik Informatika', 'willi1.jpg'),
+(4, 'David Beckham', '74452033', 'beckham1975@gmail.com', 'Psikologi', 'BKZKTrQCAAAt12l.jpg'),
+(5, 'Nike Ardilla', '54029399', 'nikeardilla75@gmail.com', 'Hukum', 'gadis_ayu.jpg'),
+(6, 'Serly', '34002877', 'delvi.serly@gmail.com', 'Teknik Informatika', 'IMG-20220901-WA0001.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- pertemuan10/index.php
+++ pertemuan10/index.php
@@ -15,6 +15,9 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 
     <h1>Daftar Mahasiswa</h1>
 
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
     <table border="1" cellpadding="10" cellspacing="0">
 
         <tr>
@@ -33,7 +36,7 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
             <td><?= $i; ?></td>
             <td>
                 <a href="">Ubah</a> |
-                <a href="">Hapus</a>
+                <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?');">Hapus</a>
             </td>
             <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
             <td><?= $row["nim"]; ?></td>
