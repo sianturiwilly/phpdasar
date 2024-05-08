34922d10d3b64506aec492c47ee16b1687e26d44
sianturiwilly <prajudiwilliam10@gmail.com>
2024-05-08 19:09:10 +0700

    Commit keempat

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -5,7 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
-    <link rel="stylesheet" href="style.css" />
+    <style>
+        .warna-baris {
+            background-color: silver;
+        }
+    </style>
 </head>
 
 <body>
